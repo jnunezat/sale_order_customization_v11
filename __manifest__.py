2af7d02c4b13cd8d144a90c8f93a018e86dff8b6
jnunezat <jeremi.nunez@apuestatotal.com>
2025-09-01 09:33:29 -0500

    fix

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name' : 'Sale Order Customization',
-    'version' : '3.0',
+    'version' : '4.0',
     'author':'',
     'category': 'Sales',
     'description': """
